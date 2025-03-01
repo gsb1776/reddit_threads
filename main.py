3e17bce140518dc3e09e38033b6a7a76af94322f
gsb1776 <gray_bittker@brown.edu>
2025-02-28 22:42:00 -0500

    bug fixes

--- main.py
+++ main.py
@@ -14,15 +14,13 @@ reddit = praw.Reddit(
     user_agent="userinfo",
 )
 
-print(reddit.user.me())
-
 # List target subreddits
-subreddits = ["politics", "economiccollapse", "antiwork"]
+subreddits = ["economiccollapse", "politics", "capitolconsequences"]
 subreddits = [item.lower() for item in subreddits]
 
 # Set some parameters
 time_limit = "year"
-count = 3
+count = 20
 
 # Subreddit info will be stored as a dataframe with:
 # top_subs, a dict with ["subreddit": karma]
@@ -42,34 +40,35 @@ for sub in subreddits:
     # Iterate through authors, getting their top subreddits and their karma 
     top_subs = {}
     for author in authors:
-        # Get the top subreddits - COULD BE BLOCKED, need a check for this
-        url = "https://www.reddit.com/user/" + author.name + "/top_karma_subreddits.json"
-        time.sleep(1)
-        response = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
-        # Load data as json 
-        data = json.loads(response.text)
-        # Iterate through all communities and add tuple
-        if data is not None and data != {}:
-            # Iterate by community
-            for community in data["data"]:
-                # Avoid double counting
-                if community["display_name"].lower() != sub:
-                    #print(community["display_name"])
-                    #print(community)
-                    name = community["display_name"].lower()
-                    karma = community["link_karma"] + community["comment_karma"]
-                    if name in top_subs:
-                        # Update stats
-                        top_subs[name] += karma
-                    else:
-                        # Or create entry
-                        top_subs[name] = (karma)
+        if author != None:
+            # Get the top subreddits - COULD BE BLOCKED, need a check for this
+            url = "https://www.reddit.com/user/" + author.name + "/top_karma_subreddits.json"
+            time.sleep(1)
+            response = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
+            # Load data as json 
+            data = json.loads(response.text)
+            # Iterate through all communities and add tuple
+            if data is not None and data != {}:
+                # Iterate by community
+                for community in data["data"]:
+                    # Avoid double counting
+                    if community["display_name"].lower() != sub:
+                        #print(community["display_name"])
+                        #print(community)
+                        name = community["display_name"].lower()
+                        karma = community["link_karma"] + community["comment_karma"]
+                        if name in top_subs:
+                            # Update stats
+                            top_subs[name] += karma
+                        else:
+                            # Or create entry
+                            top_subs[name] = (karma)
     print(top_subs)
     # sort it
     top_subs = {k: v for k, v in sorted(top_subs.items(), key=lambda item: item[1], reverse=True)}
     # Get the user count for the subreddit
     url = "https://www.reddit.com/r/" + sub + "/about.json"
-    time.sleep(1)
+    time.sleep(3)
     response = requests.get(url, headers = {'User-agent': 'your bot 0.1'})
     data = json.loads(response.text)["data"]
     new_row = (sub, top_subs, data["subscribers"])
